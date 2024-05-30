6d477b59a9ced66bda0a6b727361dc9a652fce31
Marius Olariu <marius.olariu@diffblue.com>
2024-05-30 11:58:04 +0100

    update

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,6 @@ import './../styles/Card.css';
 
 const Card = ({ photo, name }) => (
   <div className="card">
-    /* eslint-disable-next-line jsx-a11y/img-redundant-alt */
     <img src={photo} alt="an image of an officer of the masonic lodge" className="card-photo" />
     <div className="card-name">
       <h2>{name}</h2>
