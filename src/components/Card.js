4580ed3e1a6985bbc5c05aa3bb78d2b5dc5359a9
Marius Olariu <marius.lucian.olariu@gmail.com>
2024-08-17 15:57:10 +0100

    WIP officers

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import './../styles/Card.css';
+import React from "react";
+import "./../styles/Card.css";
 
-const Card = ({ photo, name }) => (
+const Card = ({ photo, name, position }) => (
   <div className="card">
-    <img src={photo} alt="an image of an officer of the masonic lodge" className="card-photo" />
+    <img
+      src={photo}
+      alt="an image of an officer of the masonic lodge"
+      className="card-photo"
+    />
     <div className="card-name">
       <h2>{name}</h2>
+      <p>{position}</p>
     </div>
   </div>
 );
--- src/components/Officers.js
+++ src/components/Officers.js
@@ -6,8 +6,12 @@ const Officers = () => (
     <h2>Officers</h2>
     <Card
       photo="https://raw.githubusercontent.com/mariusolariu/raf9456.com/main/src/images/officers/peter.jpeg"
-      name="W.M. Peter Wilkinson"
+      name="Peter Wilkinson"
+      position="Worshipful Master"
     />
+    <Card name="Neil Merchant" position="Secretary" />
+    <Card name="Roger Millwater" position="Treasurer" />
+    <p style={{ marginBottom: "4rem" }}> </p>
   </section>
 );
 
