d094ff572f91356fd6813f68d9cb984837ca6f62
Marius Olariu <marius.lucian.olariu@gmail.com>
2024-08-17 13:27:13 +0100

    Adjusted the sizes of the header text

--- src/components/MyHeader.js
+++ src/components/MyHeader.js
@@ -3,7 +3,7 @@ import React from "react";
 const MyHeader = () => (
   <header className="App-header">
     <div className="header-content">
-      <h1>
+      <h1 style={{ fontSize: "2.5rem" }}>
         <strong>Royal Air Force 9456</strong>
       </h1>
       <div className="wrapper">
