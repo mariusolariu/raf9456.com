5316277c240bedf7bab3413c11e5a6482e745870
Marius Olariu <marius.lucian.olariu@gmail.com>
2024-08-18 18:41:39 +0100

    Finished contact us page

--- src/components/About.js
+++ src/components/About.js
@@ -32,10 +32,10 @@ const About = () => (
         height="500vw"
         src="https://www.youtube.com/embed/PLwPnUvHdyk?si=-unLm_JYlXziSDqa"
         title="YouTube video player"
-        frameborder="0"
+        frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin"
-        allowfullscreen
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
       ></iframe>
     </Flex>
   </section>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { Button, Form, Input, Row, Col } from "antd";
+import { Button, Form, Input } from "antd";
+import Card from "./Card";
 
 const onFinish = (values) => {
   // Construct the mailto link
-  const mailtoLink = `mailto:${values.email}?subject=${encodeURIComponent(
-    values.subject
-  )}&body=${encodeURIComponent(values.message)}`;
+  const mailtoLink = `mailto:&#114;&#97;&#102;&#108;&#111;&#100;&#103;&#101;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;
+?subject=${encodeURIComponent(values.subject)}&body=${encodeURIComponent(
+    values.message
+  )}`;
 
   // Open the email client
   window.location.href = mailtoLink;
@@ -18,112 +20,137 @@ const { TextArea } = Input;
 const Contact = () => {
   return (
     <section id="contact">
-      <Row gutter={[16, 16]} justify="center">
-        <Col xs={24} sm={12}>
-          <div>
-            <h2>Where to find us?</h2>
-            <p>
-              <strong>Address: </strong> 1621 Warwick Rd, Knowle, Solihull B93
-              9LF, United Kingdom
-            </p>
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2434.8577844743486!2d-1.7402590227370933!3d52.39112837202768!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4870b7a7f43baedd%3A0xb053ca937a870d41!2sKnowle%20Masonic%20Centre!5e0!3m2!1sen!2suk!4v1723994040732!5m2!1sen!2suk"
-              width="1000vw"
-              height="600vh"
-              style={{ border: "0" }}
-              allowfullscreen=""
-              loading="lazy"
-              referrerpolicy="no-referrer-when-downgrade"
-            ></iframe>
-          </div>
-        </Col>
-        <Col xs={24} sm={12}>
-          <div style={{ marginTop: "4rem", marginLeft: "10rem" }}>
-            <h2>Contact us</h2>
-            <Form
-              name="basic"
-              labelCol={{
-                span: 8,
-              }}
-              wrapperCol={{
-                span: 16,
-              }}
-              style={{
-                maxWidth: 600,
-              }}
-              initialValues={{
-                remember: true,
-              }}
-              onFinish={onFinish}
-              onFinishFailed={onFinishFailed}
-              autoComplete="off"
-            >
-              <Form.Item
-                label="Name"
-                name="name"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input your name",
-                  },
-                ]}
-              >
-                <Input />
-              </Form.Item>
+      <div>
+        <h2>Where to find us?</h2>
+        <p>
+          <strong>Meeting address: </strong> 1621 Warwick Rd, Knowle, Solihull
+          B93 9LF, United Kingdom
+        </p>
+        <p>
+          <strong>RAF 9456 correspondence address: </strong>{" "}
+          &#80;&#101;&#116;&#101;&#114;&#32;&#87;&#105;&#108;&#107;&#105;&#110;&#115;&#111;&#110;&#44;&#32;&#82;&#65;&#70;&#32;&#57;&#52;&#53;&#54;&#44;&#32;&#52;&#53;&#32;&#71;&#114;&#101;&#101;&#110;&#104;&#105;&#108;&#108;&#32;&#82;&#111;&#97;&#100;&#44;&#32;&#67;&#111;&#97;&#108;&#118;&#105;&#108;&#108;&#101;&#44;&#32;&#76;&#69;&#54;&#55;&#52;&#82;&#76;&#46;
+        </p>
+        <p></p>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2434.8577844743486!2d-1.7402590227370933!3d52.39112837202768!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4870b7a7f43baedd%3A0xb053ca937a870d41!2sKnowle%20Masonic%20Centre!5e0!3m2!1sen!2suk!4v1723994040732!5m2!1sen!2suk"
+          width="1000vw"
+          height="600vh"
+          style={{ border: "0" }}
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
 
-              <Form.Item
-                label="Email"
-                name="email"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input your email",
-                  },
-                ]}
-              >
-                <Input />
-              </Form.Item>
+      <div style={{ marginTop: "4rem" }}>
+        <h2> Key Contacts</h2>
+        <Card
+          photo="./images/wmPeter.jpg"
+          name="Peter Wilkinson"
+          position="Worshipful Master"
+          phoneNumber="&#48;&#55;&#57;&#51;&#48;&#32;&#51;&#51;&#48;&#49;&#50;&#53;"
+          email="&#114;&#97;&#102;&#108;&#111;&#100;&#103;&#101;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;"
+        />
 
-              <Form.Item
-                label="Subject"
-                name="subject"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input a subject",
-                  },
-                ]}
-              >
-                <Input />
-              </Form.Item>
+        <Card
+          photo="./images/nmAvatar.png"
+          name="Neil Merchant"
+          position="Secretary"
+          email="&#115;&#101;&#99;&#114;&#101;&#116;&#97;&#114;&#121;&#114;&#97;&#102;&#57;&#52;&#53;&#54;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;"
+        />
 
-              <Form.Item
-                label="Message"
-                name="message"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input a message",
-                  },
-                ]}
-              >
-                <TextArea rows={6} />
-              </Form.Item>
+        <Card
+          photo="./images/rmAvatar.png"
+          name="Roger Millwater"
+          position="Treasurer"
+          email="&#116;&#114;&#101;&#97;&#115;&#117;&#114;&#101;&#114;&#114;&#97;&#102;&#57;&#52;&#53;&#54;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;"
+        />
+      </div>
 
-              <Form.Item
-                wrapperCol={{
-                  offset: 8,
-                  span: 16,
-                }}
-              >
-                <Button type="primary" htmlType="submit">
-                  Submit
-                </Button>
-              </Form.Item>
-            </Form>
-          </div>
-        </Col>
-      </Row>
+      <div style={{ marginTop: "4rem" }}>
+        <h2>Contact us</h2>
+        <Form
+          name="basic"
+          labelCol={{
+            span: 8,
+          }}
+          wrapperCol={{
+            span: 16,
+          }}
+          style={{
+            maxWidth: 600,
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Name"
+            name="name"
+            rules={[
+              {
+                required: true,
+                message: "Please input your name",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: "Please input your email",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Subject"
+            name="subject"
+            rules={[
+              {
+                required: true,
+                message: "Please input a subject",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Message"
+            name="message"
+            rules={[
+              {
+                required: true,
+                message: "Please input a message",
+              },
+            ]}
+          >
+            <TextArea rows={6} />
+          </Form.Item>
+
+          <Form.Item
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
     </section>
   );
 };
