bfb3b3cc97271abf093b02ef2e386be25a038985
Marius Olariu <marius.lucian.olariu@gmail.com>
2024-08-31 20:30:54 +0100

    Fix footer

--- src/components/MyFooter.js
+++ src/components/MyFooter.js
@@ -6,51 +6,51 @@ const MyFooter = () => (
     <div className="flexFooter">
       <p>&copy; 2024 Warwickshire Royal Air Force 9456</p>
 
-      <ul className="footerList">
-        <li>
-          <a
-            href="https://www.facebook.com/RAFLodge"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Facebook Page
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.facebook.com/groups/471856354289594"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Facebook Group
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://x.com/raf9456"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            X (Twitter)
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.linkedin.com/groups/9162009"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Linkedin Group
-          </a>
-        </li>
-      </ul>
-
       <img
         className="footer-logo"
         src="./images/raf9456LogoMini.png"
         alt="logo of lodge"
       ></img>
     </div>
+
+    <ul className="footerList">
+      <li>
+        <a
+          href="https://www.facebook.com/RAFLodge"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Facebook Page
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://www.facebook.com/groups/471856354289594"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Facebook Group
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://x.com/raf9456"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          X (Twitter)
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://www.linkedin.com/groups/9162009"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Linkedin Group
+        </a>
+      </li>
+    </ul>
 
     <p className="disclaimerP">
       Please note: We cannot guarantee that any link or feed from this site has
