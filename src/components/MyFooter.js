2a41373e5822fa6f0528cbb152bbab9d769c4c2e
Marius Olariu <marius.lucian.olariu@gmail.com>
2024-08-31 17:27:50 +0100

    Implment footer

--- src/components/MyFooter.js
+++ src/components/MyFooter.js
@@ -3,23 +3,62 @@ import "../styles/Footer.css";
 
 const MyFooter = () => (
   <footer>
-    <p>&copy; 2024 Royal Air Force 9456</p>
-    <ul>
-      <li>
-        <a href="https://facebook.com">Facebook</a>
-      </li>
-      <li>
-        <a href="https://twitter.com">Twitter</a>
-      </li>
-      <li>
-        <a href="https://instagram.com">Instagram</a>
-      </li>
-    </ul>
-    <img
-      className="footer-logo"
-      src="./images/raf9456LogoMini.png"
-      alt="logo of lodge"
-    ></img>
+    <div className="flexFooter">
+      <p>&copy; 2024 Royal Air Force 9456</p>
+
+      <ul className="footerList">
+        <li>
+          <a
+            href="https://www.facebook.com/RAFLodge"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Facebook Page
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.facebook.com/groups/471856354289594"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Facebook Group
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://x.com/raf9456"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            X (Twitter)
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/groups/9162009"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Linkedin Group
+          </a>
+        </li>
+      </ul>
+
+      <img
+        className="footer-logo"
+        src="./images/raf9456LogoMini.png"
+        alt="logo of lodge"
+      ></img>
+    </div>
+
+    <p className="disclaimerP">
+      Please note: We cannot guarantee that any link or feed from this site has
+      been approved by the United Grand Lodge of England or the Provincial Grand
+      Lodge of Warwickshire. Neither can we guarantee that any subsequent links
+      from their pages are recognised by, or have the approval of the United
+      Grand Lodge of England or the Provincial Grand Lodge of Warwickshire.
+    </p>
   </footer>
 );
 
