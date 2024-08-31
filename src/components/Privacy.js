f070a7059c6c163e61ee6fd277b5677ee2e8045a
Marius Olariu <marius.lucian.olariu@gmail.com>
2024-08-31 15:38:11 +0100

    Privacy policy page

--- src/components/Privacy.js
+++ src/components/Privacy.js
@@ -2,9 +2,22 @@ import React from "react";
 
 const Privacy = () => {
   return (
-    <>
-      <h1>TODO: privacy</h1>
-    </>
+    <div style={{ marginLeft: "1rem", marginBottom: "24rem" }}>
+      <h3>Who we are</h3>
+      <p>Our website address is: https://raf9456.com</p>
+      <h3>Embedded content from other websites</h3>
+      <p>
+        Articles on this site may include embedded content (e.g. videos, images,
+        articles, etc.). Embedded content from other websites behaves in the
+        exact same way as if the visitor has visited the other website.
+      </p>
+      <p>
+        These websites may collect data about you, use cookies, embed additional
+        third-party tracking, and monitor your interaction with that embedded
+        content, including tracking your interaction with the embedded content
+        if you have an account and are logged in to that website.
+      </p>
+    </div>
   );
 };
 
